d46130542a49437e6b6b87b370b6a3b8141f18c9
Riki Montgomery <11416397+mostriki@users.noreply.github.com>
2019-06-10 08:08:09 -0700

    improvements

---
+++ setup-symlinks.sh
@@ -0,0 +1,34 @@
+
+# Variables
+dir=~/.dotfiles
+files=".zshrc .eslintrc.js .gitconfig .vimrc .hyper.js"
+
+echo ""
+echo "- - - - - - - - - -"
+echo ""
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+
+echo ""
+echo "- - - - - - - - - -"
+echo ""
+
+# Create symlinks
+for file in $(echo $files); do
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/$file
+  echo "- - -"
+done
+
+echo ""
+echo "- - - - - - - - - -"
+echo ""
+echo "All done. Have a great day :-*"
+echo " "
+echo "- - - - - - - - - -"
+echo ""
+
+# initialize new settings
+source ~/.zshrc
